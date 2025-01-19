3f35346cd9b53f04a32ade568b83aab9c110dfd5
Jake-Moore <jakealexandermoore@icloud.com>
2025-01-19 00:26:48 -0800

    beta 10 - fix getOrNull because Success interfaces are not the same
    Took 3 minutes

--- src/main/kotlin/com/kamikazejam/datastore/base/async/result/AsyncResult.kt
+++ src/main/kotlin/com/kamikazejam/datastore/base/async/result/AsyncResult.kt
@@ -17,32 +17,25 @@ sealed interface AsyncResult<T>
 //                    Results Providing Store                   //
 // ------------------------------------------------------------ //
 
-sealed interface StoreResult<K, X : Store<X, K>> : AsyncResult<X> {
-    data class Success<K, X : Store<X, K>>(val value: X) : StoreResult<K, X>
-    data class Failure<K, X : Store<X, K>>(val error: AsyncHandlerException) : StoreResult<K, X>
-
-    fun getOrNull(): X? {
-        return when (this) {
-            is Success -> value
-            else -> null
-        }
-    }
-}
+sealed interface StoreResult<K, X : Store<X, K>> : AsyncResult<X>
 
 sealed interface ReadResult<K, X : Store<X, K>> : StoreResult<K, X> {
     data class Success<K, X : Store<X, K>>(val value: X) : ReadResult<K, X>
     data class Failure<K, X : Store<X, K>>(val error: AsyncHandlerException) : ReadResult<K, X>
     class Empty<K, X : Store<X, K>> : ReadResult<K, X>
+    fun getOrNull(): X? = if (this is Success) value else null
 }
 
 sealed interface CreateResult<K, X : Store<X, K>> : StoreResult<K, X> {
     data class Success<K, X : Store<X, K>>(val value: X) : CreateResult<K, X>
     data class Failure<K, X : Store<X, K>>(val error: AsyncHandlerException) : CreateResult<K, X>
+    fun getOrNull(): X? = if (this is Success) value else null
 }
 
 sealed interface UpdateResult<K, X : Store<X, K>> : StoreResult<K, X> {
     data class Success<K, X : Store<X, K>>(val value: X) : UpdateResult<K, X>
     data class Failure<K, X : Store<X, K>>(val error: AsyncHandlerException) : UpdateResult<K, X>
+    fun getOrNull(): X? = if (this is Success) value else null
 }
 
 
@@ -54,7 +47,7 @@ sealed interface UpdateResult<K, X : Store<X, K>> : StoreResult<K, X> {
 sealed interface DeleteResult : AsyncResult<Boolean> {
     data class Success(val value: Boolean) : DeleteResult
     data class Failure(val error: AsyncHandlerException) : DeleteResult
-
+    fun getOrNull(): Boolean? = if (this is Success) value else null
     fun get(def: Boolean = false): Boolean {
         return when (this) {
             is Success -> value
@@ -71,7 +64,7 @@ sealed interface DeleteResult : AsyncResult<Boolean> {
 sealed interface BoolResult : AsyncResult<Boolean> {
     data class Success(val value: Boolean) : BoolResult
     data class Failure(val error: AsyncHandlerException) : BoolResult
-
+    fun getOrNull(): Boolean? = if (this is Success) value else null
     fun get(def: Boolean = false): Boolean {
         return when (this) {
             is Success -> value
@@ -84,11 +77,5 @@ sealed interface ReadIdResult<K> : AsyncResult<K> {
     data class Success<K>(val value: K) : ReadIdResult<K>
     data class Failure<K>(val error: AsyncHandlerException) : ReadIdResult<K>
     class Empty<K> : ReadIdResult<K>
-
-    fun getOrNull(): K? {
-        return when (this) {
-            is Success -> value
-            else -> null
-        }
-    }
+    fun getOrNull(): K? = if (this is Success) value else null
 }
