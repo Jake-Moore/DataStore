30a93f3c7a153b0ca90abdcb93cb0d29ac90b781
Jake-Moore <jakealexandermoore@icloud.com>
2025-02-03 00:20:22 -0800

    0.2.0.alpha.2-SNAPSHOT - DataStoreScope extensions for running things synchronously. Will greatly reduce code bloat by having these here for easy use.

--- src/main/kotlin/com/kamikazejam/datastore/base/coroutine/DataStoreScope.kt
+++ src/main/kotlin/com/kamikazejam/datastore/base/coroutine/DataStoreScope.kt
@@ -1,11 +1,37 @@
+@file:Suppress("unused", "UnusedReceiverParameter")
+
 package com.kamikazejam.datastore.base.coroutine
 
+import com.kamikazejam.datastore.DataStoreSource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 @Suppress("unused")
 interface DataStoreScope : CoroutineScope {
     override val coroutineContext: CoroutineContext
     get() = Dispatchers.IO
 }
+
+fun DataStoreScope.runSync(runnable: Runnable) {
+    val plugin = DataStoreSource.get()
+    plugin.server.scheduler.runTask(plugin, runnable)
+}
+
+suspend fun DataStoreScope.runSyncBlocking(block: () -> Unit) {
+    return suspendCoroutine { continuation ->
+        val plugin = DataStoreSource.get()
+        plugin.server.scheduler.runTask(plugin) {
+            try {
+                block()
+                continuation.resume(Unit)
+            } catch (e: Exception) {
+                continuation.resumeWithException(e)
+            }
+        }
+    }
+}
+
